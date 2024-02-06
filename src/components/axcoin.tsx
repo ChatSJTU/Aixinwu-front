ce893efccd90963be619c8fb502b15aa1143e00c
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-02-06 16:48:25 +0800

    feat: add an icon design of axcoin

---
+++ src/components/axcoin.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { HeartTwoTone } from '@ant-design/icons';
+
+interface CustomHeartIconProps {
+  size?: number; 
+  color?: string;
+  style?: React.CSSProperties;
+}
+
+export const AxCoin: React.FC<CustomHeartIconProps> = ({ 
+    size = 22,
+    color = "#eb2f96",
+    style
+}) => {
+    const borderWidth = size / 16;
+
+    return (
+        <div 
+            style={{ 
+                fontSize: size / 1.8,
+                backgroundColor: '#fff',
+                width: size,
+                height: size,
+                borderRadius: '50%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                border: `${borderWidth}px solid ${color}`, 
+                ...style
+            }}
+        >
+            <HeartTwoTone twoToneColor={color}/>
+        </div>
+    );
+};
+
