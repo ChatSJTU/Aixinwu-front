acff2a07f4e76f6c1c81485a456fc173e652b521
Ichigo2315 <csy_ichigo@sjtu.edu.cn>
2024-02-13 13:16:51 +0800

    feat(detailpage): support image url list

--- src/pages/products/single/[id].tsx
+++ src/pages/products/single/[id].tsx
@@ -1,4 +1,4 @@
-import { Image, Spin, Divider, Row, Col, Button, Typography } from 'antd'
+import { Image, Spin, Divider, Row, Col, Button, Typography, Carousel } from 'antd'
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
 import Head from "next/head";
@@ -8,7 +8,7 @@ import { AxCoin } from '@/components/axcoin';
 const { Title, Text, Link } = Typography;
 
 interface ProductDetails {
-    image_url: string;
+    image_url: string[]; // 长度至少为1
     product_name: string;
     detailed_product_name?: string;
     desc: string;
@@ -30,7 +30,9 @@ const ProductDetailsPage: React.FC = () => {
                 // 以下为一个示例
                 const response = {
                     data: {
-                        image_url: "https://aixinwu.sjtu.edu.cn/uploads/product/6395/202203_347.jpg",
+                        image_url: ["https://aixinwu.sjtu.edu.cn/uploads/product/6395/202203_347.jpg",
+                            "https://aixinwu.sjtu.edu.cn/uploads/product/6394/202203_347.jpg",
+                            "https://aixinwu.sjtu.edu.cn/uploads/product/6457/202306_347.jpg"],
                         product_name: "新航道2023考研政治900题",
                         detailed_product_name: "新航道考研政治2023年徐之明思想政治理论金榜书900题",
                         desc: "//此部分后续可更换为嵌入html代码，需注意DOM-based XSS\n\n商品名称：新航道2023考研政治900题\n出版社：世界知识出版社\n本书有以下特点：\n1.根据考试大纲、2021版新教材编写，考点覆盖，重点突出，讲解详实。\n2.中国人民大学教授、原考研政治命题专家主编。\n3.偶数页题目、奇数页答案对应编排，人性化设计方便考生刷题。",
@@ -63,12 +65,18 @@ const ProductDetailsPage: React.FC = () => {
             <div className='container'>
                 <Row>
                     <Col span={6}>
-                        <Image
-                            src={productDetails.image_url}
-                            alt={productDetails.product_name}
-                            width={168}
-                            height={168}
-                        />
+                        <Carousel>
+                            {productDetails.image_url.map((url, index) => (
+                                <div key={index}>
+                                    <Image
+                                        src={url}
+                                        alt={`图片预览 ${index + 1}`}
+                                        width={256}
+                                        height={256}
+                                    />
+                                </div>
+                            ))}
+                        </Carousel>
                     </Col>
                     <Col span={18} style={{ paddingLeft: '250px' }}>
 
