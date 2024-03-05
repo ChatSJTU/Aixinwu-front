4389197c4861798313506bf6103d8ee44d409efc
1357310795 <1357310795@qq.com>
2024-03-05 17:04:57 +0800

    feat: load graphql endpoint from .env file

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 
 const create_graphql_client = () => {
-  const httpLink = new HttpLink({uri: "http://localhost:8000/graphql/"})
+  const httpLink = new HttpLink({uri: process.env.NEXT_PUBLIC_GRAPHQL_URL})
   return new ApolloClient({
     link: httpLink,
     cache: new InMemoryCache()
