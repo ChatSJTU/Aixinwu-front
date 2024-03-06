aae31edc303ac9c92001ef17534eef9809475dbb
1357310795 <1357310795@qq.com>
2024-03-06 10:47:19 +0800

    feat(app): use mounted state and splashscreen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,23 @@ import '@/styles/globals.scss';
 import MainLayout from '@/components/layout';
 
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import SplashScreen from '@/components/splash-screen';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) 
+    return (
+      <>
+        <SplashScreen></SplashScreen>
+      </>
+    );
+  
   return (
       <MainLayout>
         <Component {...pageProps} />
