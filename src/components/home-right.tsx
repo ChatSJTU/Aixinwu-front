c6c6fae436692fcc43b6d6d59ce24131d063067f
Ichigo2315 <csy_ichigo@sjtu.edu.cn>
2024-03-06 23:15:53 +0800

    feat(item-grid): add product-grid component and product preview card

--- src/components/home-right.tsx
+++ src/components/home-right.tsx
@@ -1,4 +1,6 @@
 import { Typography } from "antd";
+import ProductGrid from "@/components/product-grid";
+import { ProductSummaryExampleList } from "@/models/products";
 
 const { Title } = Typography;
 
@@ -7,6 +9,7 @@ export const HomeRightContent = () => {
         <>
             <div className="container basic-card">
                 <Title level={5}>热门置换</Title>
+                <ProductGrid products={[...ProductSummaryExampleList, ...ProductSummaryExampleList, ...ProductSummaryExampleList]} />
             </div>
             <div className="container basic-card">
                 <Title level={5}>租赁专区</Title>
--- src/components/product-preview-card.tsx
+++ src/components/product-preview-card.tsx
@@ -1,71 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { Card, Image, Typography, Row, Col } from 'antd'
-import { ShoppingCartOutlined } from '@ant-design/icons';
+import React from "react";
+import { Card, Image, Typography, Row, Col, Flex } from 'antd'
+import { ShoppingCartOutlined, PayCircleOutlined } from '@ant-design/icons';
 
-import { ProductSummary } from "@/models/products";
-import { AxCoin } from "@/components/axcoin";
+import { ProductSummaryProps } from "@/models/products";
 
 const { Title, Text } = Typography;
 
 export const ProductPreviewCard: React.FC<ProductSummaryProps> = ({ productSummary }) => {
     const { image_url, product_id, product_name, detailed_product_name, cost, stock } = productSummary;
 
-    const [hovered, setHovered] = useState(false);
-
     return (
         <div
-            onMouseOver={() => setHovered(true)}
-            onMouseOut={() => setHovered(false)}
-            style={{
-                maxWidth: 200,
-                borderRadius: '8px',
-                overflow: 'hidden',
-                transition: 'border-color 0.3s, box-shadow 0.3s',
-                borderColor: hovered ? '#B3D4FC' : 'transparent',
-                borderStyle: 'solid',
-                borderWidth: '1px',
-                position: 'relative',
-                zIndex: hovered ? 1 : 'auto',
-            }}
+            className="preview-card-container"
         >
-            <a target="_blank" href={`/products/single/${product_id}`}>
-                
-            <Card
-                hoverable
-                style={{
-                    maxWidth: 200,
-                    transition: 'box-shadow 0.3s',
-                    border: 'none'
-                }}
-                cover={<Image alt={product_name} src={image_url[0]} preview={false} />}
-            >
-                <div style={{ padding: '10px' }}>
-                    <Title level={5}>{product_name.length < 20 ? product_name : `${product_name.substring(0,20)}...`}</Title>
-                    <Row justify="space-between" align="middle">
-                        <Col style={{display:'flex', alignItems:'flex-start'}}>
-                            <ShoppingCartOutlined size={14}/>
-                            <Text>{`: ${stock}`}</Text>
-                        </Col>
-                        <Col style={{display:'flex'}}>
-                            <AxCoin size={14}/>
-                            <Text style={{ color: '#eb2f96' }}>{`: ${cost.toFixed(2)}`}</Text>
-                        </Col>
-                    </Row>
-                    <div
-                        style={{
-                            maxHeight: hovered ? '100px' : '0',
-                            overflow: 'hidden',
-                            transition: 'max-height 0.3s ease',
-                        }}
-                    >
+            <a target="_blank" href={`/books/${product_id}`}>
+                <Card
+                    hoverable
+                    className="preview-card"
+                    cover={<Image className='preview-card-cover' alt={product_name} src={image_url[0]} preview={true} />}
+                >
+                    <div>
+                        <span className='preview-card-title'>{product_name}</span>
+                        <Flex justify="space-between" align="center">
+                            <div>
+                                <ShoppingCartOutlined />
+                                <Text style={{marginLeft:'3px'}}>{` ${stock}`}</Text>
+                            </div>
+                            <div>
+                                <PayCircleOutlined/>
+                                <Text strong style={{marginLeft:'4px'}}>{`${cost.toFixed(2)}`}</Text>
+                            </div>
+                        </Flex>
                         {detailed_product_name &&
-                            <Text type="secondary">
+                            <Text type="secondary" className="two-line-ellipsis">
                                 {detailed_product_name.length < 30 ? detailed_product_name : `${detailed_product_name.substring(0, 30)}...`}
                             </Text>
                         }
                     </div>
-                </div>
-            </Card>
+                </Card>
             </a>
         </div>
     );
