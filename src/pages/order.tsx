fad6e9de4d83af4f7864b05ff8f33ca3d117672a
ush11o <1242291955@qq.com>
2024-03-10 21:27:32 +0800

    feat(order page): using antd.Table

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,8 +1,22 @@
 import Head from "next/head";
-import {Col, Row, List, Space, Input, Avatar, Skeleton, InputNumber, Typography, Image, Button} from "antd";
+import {Col, Row, List, Space, Input, Avatar, Skeleton, InputNumber, Typography, Image, Button, Table} from "antd";
+const { Column } = Table;
 import {AxCoin} from "@/components/axcoin";
 import React, {useState} from "react";
-const { Title, Text, Link } = Typography;
+const { Title, Text, Link, Paragraph } = Typography;
+
+interface product {
+    id: number,
+    itemNumber: number,
+    image_url: string[],
+    pruduct : {
+        product_name: string,
+        detailed_product_name: string
+    }
+    cost: number
+    stock: number
+    subtotal: number
+}
 
 
 function getProductSummary(id: number, itemNumber: number) {
@@ -12,12 +26,13 @@ function getProductSummary(id: number, itemNumber: number) {
         image_url: ["https://aixinwu.sjtu.edu.cn/uploads/product/6395/202203_347.jpg",
             "https://aixinwu.sjtu.edu.cn/uploads/product/6394/202203_347.jpg",
             "https://aixinwu.sjtu.edu.cn/uploads/product/6457/202306_347.jpg"],
-        product_name: "新航道2023考研政治900题",
-        detailed_product_name: "新航道考研政治2023年徐之明思想政治理论金榜书900题新航道考",
-        desc: "//此部分后续可更换为嵌入html代码，需注意DOM-based XSS\n\n商品名称：新航道2023考研政治900题\n出版社：世界知识出版社\n本书有以下特点：\n1.根据考试大纲、2021版新教材编写，考点覆盖，重点突出，讲解详实。\n2.中国人民大学教授、原考研政治命题专家主编。\n3.偶数页题目、奇数页答案对应编排，人性化设计方便考生刷题。",
+        product: {
+            product_name: "新航道2023考研政治900题TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT",
+            detailed_product_name: "新航道考研政治2023年徐之明思想政治理论金榜书900题新航道考治2023年徐之明思想政治理论金榜书900题新航道考治2023年徐之明思想政治理论金榜书900题新航道考治2023年徐之明思想政治理论金榜书900题新航道考AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        },
         cost: 24,
         stock: 907,
-
+        subtotal: 0
     });
 }
 
@@ -86,6 +101,129 @@ const OrderPageView = () => {
             <Row>
                 <Col span={18}>
                     <div className={"container"}>
+                        <Table
+                            dataSource={listProducts}
+                        >
+                            <Column
+                                title={"图片"}
+                                dataIndex={"image_url"}
+                                width={"15%"}
+                                align={"center"}
+                                render={(urls: string[]) => (<Image src={urls[0]} preview={false} />)}
+                            />
+                            <Column
+                                title={"商品"}
+                                dataIndex={"product"}
+                                ellipsis={true}
+                                width={"35%"}
+                                render={(product) => (
+                                    <Space
+                                        direction={"vertical"}
+                                        style={{maxWidth: "100%"}}
+                                    >
+                                        <span style={{
+                                            fontSize: "14px", fontWeight: "bold",
+                                            overflow: "hidden", maxWidth: "100%"}}>
+
+                                        </span>
+                                        <Text
+                                            strong={true}
+                                            ellipsis={true}
+                                        >
+                                            {product.product_name}
+                                        </Text>
+                                        <Paragraph
+                                            type={"secondary"}
+                                            style={{
+                                                fontSize: "12px", color: "gray",
+                                                whiteSpace: "pre-wrap"
+                                            }}
+                                            ellipsis={{rows: 2, expandable: false}}
+                                        >
+                                            {product.detailed_product_name}
+                                        </Paragraph>
+                                    </Space>
+                                )}
+                            />
+                            <Column
+                                title={"单价"}
+                                dataIndex={"cost"}
+                                width={"10%"}
+                                align={"center"}
+                                render={(cost) => (
+                                    <div style={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
+                                        <AxCoin size={16}/> <span style={{color: '#eb2f96'}}>{cost}</span>
+                                    </div>
+                                )}
+                            />
+                            <Column
+                                title={"数量"}
+                                dataIndex={"itemNumber"}
+                                width={"20%"}
+                                align={"center"}
+                                ellipsis={true}
+                                render={(_: any, x: product) => (
+                                    <Space
+                                        direction={"vertical"}
+                                        style={{width: '100%'}}
+                                    >
+                                        <Space.Compact style={{width: '100%'}}>
+                                            <Button
+                                                style={{width: '20%', padding: 0}}
+                                                size={"small"}
+                                                type={"ghost"}
+                                                // style={{paddingLeft: '10%', paddingRight: '10%'}}
+                                                onClick={()=>onItemNumberMinus(x.id)}
+                                                disabled={x.itemNumber <= 1}
+                                            >
+                                                -
+                                            </Button>
+                                            <InputNumber
+                                                style={{width: '60%'}}
+                                                size={"small"}
+                                                min={1} max={x.stock} value={x.itemNumber}
+                                                controls={false}
+                                                onChange={(value) => onItemNumberChange(x.id, value)}
+                                            />
+                                            <Button
+                                                style={{width: '20%', padding: 0}}
+                                                size={"small"}
+                                                type={"ghost"}
+                                                // style={{paddingLeft: '10%', paddingRight: '10%'}}
+                                                onClick={()=>onItemNumberPlus(x.id)}
+                                                disabled={x.itemNumber >= x.stock}
+                                            >
+                                                +
+                                            </Button>
+                                        </Space.Compact>
+                                        <Text style={{fontSize: '12px', color: "gray", display: 'flex', alignItems: "center", justifyContent: "center"}}>
+                                            库存：{x.stock}
+                                        </Text>
+                                    </Space>
+                                )}
+                            />
+                            <Column
+                                title={"小计"}
+                                align={"center"}
+                                width={"10%"}
+                                render={(_: any, x: product) => (
+                                    <div style={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
+                                        <AxCoin size={16}/> <span style={{color: '#eb2f96'}}>{x.itemNumber * x.cost}</span>
+                                    </div>
+                                )}
+                            />
+                            <Column
+                                title={"    "}
+                                align={"center"}
+                                width={"10%"}
+                                ellipsis={true}
+                                render={(_: any, x: product) => (
+                                    <Button style={{padding: 0}} type={"link"} onClick={(()=>onClickDelete(x.id))}>删除</Button>
+                                )}
+                            />
+                        </Table>
+                    </div>
+                    <div className={"container"}>
                         <List
                             itemLayout="horizontal"
                             dataSource={listProducts}
@@ -108,9 +246,9 @@ const OrderPageView = () => {
                                         <Row style={{width: "100vw", alignItems:"top", alignContent:"top"}} justify="space-between" align="top">
                                             <Col span={12} style={{display:"flex"}}>
                                                 <List.Item.Meta
-                                                    avatar={<Image src={item.image_url[0]} style={{maxWidth: "7vw"}}/>}
-                                                    title={item.product_name}
-                                                    description={item.detailed_product_name}
+                                                    avatar={<Image src={item.image_url[0]} style={{maxWidth: "7vw"}} />}
+                                                    title={item.product.product_name}
+                                                    description={item.product.detailed_product_name}
                                                 />
                                             </Col>
                                             <Col span={2} style={{display:"flex"}}>
@@ -126,6 +264,7 @@ const OrderPageView = () => {
                                                 <div style={{display: "flex", flexDirection: "column", alignItems:"center"}}>
                                                     <Space.Compact style={{width: '100%'}}>
                                                         <Button
+                                                            size={"small"}
                                                             style={{paddingLeft: '10%', paddingRight: '10%'}}
                                                             onClick={()=>onItemNumberMinus(item.id)}
                                                             disabled={item.itemNumber <= 1}
@@ -133,11 +272,13 @@ const OrderPageView = () => {
                                                             -
                                                         </Button>
                                                         <InputNumber
+                                                            size={"small"}
                                                             min={1} max={item.stock} value={item.itemNumber}
                                                             controls={false}
                                                             onChange={(value) => onItemNumberChange(item.id, value)}
                                                         />
                                                         <Button
+                                                            size={"small"}
                                                             style={{paddingLeft: '10%', paddingRight: '10%'}}
                                                             onClick={()=>onItemNumberPlus(item.id)}
                                                             disabled={item.itemNumber >= item.stock}
