6c22850f7106ae17f1048cb959de3ac6cb00929d
UNIkeEN <2641712741@qq.com>
2024-03-11 12:33:33 +0800

    feat(axcoin): rewrite ax-coin component
    Co-Authored-By: 1357310795 <35869804+1357310795@users.noreply.github.com>

--- src/components/axcoin.tsx
+++ src/components/axcoin.tsx
@@ -1,36 +1,73 @@
-import React from 'react';
-import { HeartTwoTone } from '@ant-design/icons';
+import React, { useContext } from 'react';
+import { Space, Typography } from "antd";
+import Icon from '@ant-design/icons';
+import AxCoinLight from '../../assets/icons/axcoin-light.svg'
+import AxCoinDark from '../../assets/icons/axcoin-dark.svg'
+import ThemeContext from "@/contexts/theme";
 
 interface AxCoinProps {
-  size?: number; 
-  color?: string;
-  style?: React.CSSProperties;
+  size?: number;                // 图标大小（可选，后续字体自适应调整）
+  style?: React.CSSProperties;  // 图标style（可选，若在此定义color将覆盖主题色）
+  value?: number;               // 价格（可选）
+  coloredValue?: boolean;       // 是否使用主题色绘制价格（可选）
+  valueStyle?: React.CSSProperties; // 价格style（可选）
+  originValue?: number          // 原价（可选，value存在时生效）
 }
 
 export const AxCoin: React.FC<AxCoinProps> = ({ 
     size = 22,
-    color = "#eb2f96",
-    style
+    style,
+    value,
+    coloredValue = false,
+    valueStyle,
+    originValue
 }) => {
-    const borderWidth = size / 16;
+
+    const themeCtx = useContext(ThemeContext);
+
+    const iconStyle = {
+        ...style,
+        color: style?.color || (themeCtx.userTheme == 'light' ? "#EB2F96" : "#CD2882"),
+        fontSize: size,
+    };
 
     return (
-        <div 
-            style={{ 
-                fontSize: size / 1.8,
-                backgroundColor: '#fff',
-                width: size,
-                height: size,
-                borderRadius: '50%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                border: `${borderWidth}px solid ${color}`, 
-                ...style
-            }}
-        >
-            <HeartTwoTone twoToneColor={color}/>
-        </div>
+        <Space align="center" style={{ height: 'auto' }}>
+            <Icon 
+                component={themeCtx.userTheme == 'light' ? AxCoinLight : AxCoinDark} 
+                style={iconStyle}
+            />
+            {value && 
+                <div style={{alignItems: 'baseline', display: 'inline-flex'}}>
+                    <p
+                        className = {coloredValue ? '' : 'primary-text'}
+                        style={{
+                            fontSize: size * 0.8,
+                            fontWeight: '500',
+                            lineHeight: `${size}px`,
+                            ...(coloredValue==true
+                                ? {color : (style?.color || (themeCtx.userTheme == 'light' ? "#EB2F96" : "#CD2882"))}
+                                : {}),
+                            ...valueStyle
+                        }}
+                    >
+                        {`${value.toFixed(2)}`}
+                    </p>
+                    {originValue && 
+                        <del
+                            className='secondary-text'
+                            style={{
+                                fontSize: size * 0.6,
+                                lineHeight: `${size}px`,
+                                marginLeft: '6px'
+                            }}
+                        >
+                            {`${originValue.toFixed(2)}`}
+                        </del>
+                    }
+                </div>
+            }
+        </Space>
     );
 };
 
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,5 +1,7 @@
 import { PageHeader } from "@/components/page-header";
 import { LoadingSpin } from "@/components/loading-spin";
+import { AxCoin } from "@/components/axcoin";
+import { Space } from "antd";
 import Head from "next/head";
 
 const TestPage = () => {
@@ -10,7 +12,14 @@ const TestPage = () => {
         </Head>
         <PageHeader title="组件测试页面" onBack/>
         <div className="container">
-            <LoadingSpin size={60} style={{color: "#cbcdd1"}} logo/>
+          <Space size="large">
+            <AxCoin size={16}/>
+            <AxCoin />
+            <AxCoin value={10}/>
+            <AxCoin value={10} originValue={100}/>
+            <AxCoin value={10} originValue={100} coloredValue/>
+            <AxCoin value={10} originValue={100} coloredValue style={{color: "#F57A25"}}/>
+          </Space>
         </div>
     </>
   );
