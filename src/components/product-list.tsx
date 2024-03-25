0af26cf11e1f3267fead09488bfa65e5b2c66603
ush11o <1242291955@qq.com>
2024-03-25 10:17:54 +0800

    feat(order): impl mobile page

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -1,4 +1,5 @@
 import {
+    Grid,
     Col,
     Row,
     List,
@@ -12,7 +13,7 @@ import {
     Menu,
     Dropdown,
     Space,
-    Table,
+    Table, Flex,
 } from 'antd';
 const { Column } = Table;
 const { Paragraph } = Typography;
@@ -25,6 +26,7 @@ import {ProductSummary} from "@/models/products";
 const { Title, Text, Link } = Typography;
 const { Panel } = Collapse;
 import type {OrderListProductsProps, OrderProduct} from "@/models/order";
+const { useBreakpoint } = Grid;
 
 
 export const OrderProductList: React.FC<OrderListProductsProps> = (
@@ -36,136 +38,247 @@ export const OrderProductList: React.FC<OrderListProductsProps> = (
         onItemNumberPlus
     }
 ) => {
+    const screens = useBreakpoint();
     // 确认订单界面，左边的一列商品信息：图、标题、单价、购买数量、小记、删除
-    return (
-
-        <div className={"container"}>
-            <Table
-                dataSource={OrderListProducts}
-            >
-                <Column
-                    title={"图片"}
-                    dataIndex={"image_url"}
-                    width={"15%"}
-                    align={"center"}
-                    render={(urls: string[]) => (<Image src={urls[0]} preview={false} />)}
-                />
-                <Column
-                    title={"商品"}
-                    dataIndex={"product"}
-                    ellipsis={true}
-                    width={"35%"}
-                    render={(product) => (
-                        <Space
-                            direction={"vertical"}
-                            style={{maxWidth: "100%"}}
-                        >
-                                        <span style={{
-                                            fontSize: "14px", fontWeight: "bold",
-                                            overflow: "hidden", maxWidth: "100%"}}>
-
-                                        </span>
-                            <Text
-                                strong={true}
-                                ellipsis={true}
+    if (screens.md) {
+        return (
+            <div className={"container"}>
+                <Table
+                    dataSource={OrderListProducts}
+                >
+                    <Column
+                        title={"图片"}
+                        dataIndex={"image_url"}
+                        width={"15%"}
+                        align={"center"}
+                        render={(urls: string[]) => (<Image src={urls[0]} preview={false}/>)}
+                    />
+                    <Column
+                        title={"商品"}
+                        dataIndex={"product"}
+                        ellipsis={true}
+                        width={"35%"}
+                        render={(product) => (
+                            <Space
+                                direction={"vertical"}
+                                style={{maxWidth: "100%"}}
                             >
-                                {product.product_name}
-                            </Text>
-                            <Paragraph
-                                type={"secondary"}
-                                style={{
-                                    fontSize: "12px", color: "gray",
-                                    whiteSpace: "pre-wrap"
-                                }}
-                                ellipsis={{rows: 2, expandable: false}}
-                            >
-                                {product.detailed_product_name}
-                            </Paragraph>
-                        </Space>
-                    )}
-                />
-                <Column
-                    title={"单价"}
-                    dataIndex={"cost"}
-                    width={"10%"}
-                    align={"center"}
-                    render={(cost) => (
-                        <div style={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
-                            <AxCoin size={22}/> <span style={{color: '#eb2f96'}}>{cost}</span>
-                        </div>
-                    )}
-                />
-                <Column
-                    title={"数量"}
-                    dataIndex={"itemNumber"}
-                    width={"20%"}
-                    align={"center"}
-                    ellipsis={true}
-                    render={(_: any, x: OrderProduct) => (
-                        <Space
-                            direction={"vertical"}
-                            style={{width: '100%'}}
-                            align={'center'}
-                        >
-                            <Space.Compact style={{width: '60%'}}>
-                                <Button
-                                    style={{width: '25%', padding: 0}}
-                                    size={"small"}
-                                    // type={"ghost"}
-                                    // style={{paddingLeft: '10%', paddingRight: '10%'}}
-                                    onClick={()=>onItemNumberMinus(x.id)}
-                                    disabled={x.itemNumber <= 1}
+                                            <span style={{
+                                                fontSize: "14px", fontWeight: "bold",
+                                                overflow: "hidden", maxWidth: "100%"
+                                            }}>
+
+                                            </span>
+                                <Text
+                                    strong={true}
+                                    ellipsis={true}
                                 >
-                                    -
-                                </Button>
-                                <InputNumber
-                                    style={{width: '50%'}}
-                                    size={"small"}
-                                    min={1} max={x.stock} value={x.itemNumber}
-                                    controls={false}
-                                    onChange={value => {
-                                        if (value !== null) {
-                                            onItemNumberChange(x.id, value);
-                                        }
+                                    {product.product_name}
+                                </Text>
+                                <Paragraph
+                                    type={"secondary"}
+                                    style={{
+                                        fontSize: "12px", color: "gray",
+                                        whiteSpace: "pre-wrap"
                                     }}
-                                />
-                                <Button
-                                    style={{width: '25%', padding: 0}}
-                                    size={"small"}
-                                    // type={"ghost"}
-                                    // style={{paddingLeft: '10%', paddingRight: '10%'}}
-                                    onClick={()=>onItemNumberPlus(x.id)}
-                                    disabled={x.itemNumber >= x.stock}
+                                    ellipsis={{rows: 2, expandable: false}}
                                 >
-                                    +
-                                </Button>
-                            </Space.Compact>
-                            <Text style={{fontSize: '12px', color: "gray", display: 'flex', alignItems: "center", justifyContent: "center"}}>
-                                库存：{x.stock}
-                            </Text>
-                        </Space>
-                    )}
-                />
-                <Column
-                    title={"小计"}
-                    align={"center"}
-                    width={"10%"}
-                    render={(_: any, x: OrderProduct) => (
-                        <div style={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
-                            <AxCoin size={22}/> <span style={{color: '#eb2f96'}}>{x.itemNumber * x.cost}</span>
-                        </div>
-                    )}
-                />
-                <Column
-                    title={"    "}
-                    align={"center"}
-                    width={"10%"}
-                    ellipsis={true}
-                    render={(_: any, x: OrderProduct) => (
-                        <Button style={{padding: 0}} type={"link"} onClick={(()=>onClickDelete(x.id))}>删除</Button>
-                    )}
-                />
-            </Table>
-        </div>
-    )
+                                    {product.detailed_product_name}
+                                </Paragraph>
+                            </Space>
+                        )}
+                    />
+                    <Column
+                        title={"单价"}
+                        dataIndex={"cost"}
+                        width={"10%"}
+                        align={"center"}
+                        render={(cost) => (
+                            <div style={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
+                                <AxCoin size={22}/> <span style={{color: '#eb2f96'}}>{cost}</span>
+                            </div>
+                        )}
+                    />
+                    <Column
+                        title={"数量"}
+                        dataIndex={"itemNumber"}
+                        width={"20%"}
+                        align={"center"}
+                        ellipsis={true}
+                        render={(_: any, x: OrderProduct) => (
+                            <Space
+                                direction={"vertical"}
+                                style={{width: '100%'}}
+                                align={'center'}
+                            >
+                                <Space.Compact style={{width: '60%'}}>
+                                    <Button
+                                        style={{width: '25%', padding: 0}}
+                                        size={"small"}
+                                        // type={"ghost"}
+                                        // style={{paddingLeft: '10%', paddingRight: '10%'}}
+                                        onClick={() => onItemNumberMinus(x.id)}
+                                        disabled={x.itemNumber <= 1}
+                                    >
+                                        -
+                                    </Button>
+                                    <InputNumber
+                                        style={{width: '50%'}}
+                                        size={"small"}
+                                        min={1} max={x.stock} value={x.itemNumber}
+                                        controls={false}
+                                        onChange={value => {
+                                            if (value !== null) {
+                                                onItemNumberChange(x.id, value);
+                                            }
+                                        }}
+                                    />
+                                    <Button
+                                        style={{width: '25%', padding: 0}}
+                                        size={"small"}
+                                        // type={"ghost"}
+                                        // style={{paddingLeft: '10%', paddingRight: '10%'}}
+                                        onClick={() => onItemNumberPlus(x.id)}
+                                        disabled={x.itemNumber >= x.stock}
+                                    >
+                                        +
+                                    </Button>
+                                </Space.Compact>
+                                <Text style={{
+                                    fontSize: '12px',
+                                    color: "gray",
+                                    display: 'flex',
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}>
+                                    库存：{x.stock}
+                                </Text>
+                            </Space>
+                        )}
+                    />
+                    <Column
+                        title={"小计"}
+                        align={"center"}
+                        width={"10%"}
+                        render={(_: any, x: OrderProduct) => (
+                            <div style={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
+                                <AxCoin size={22}/> <span style={{color: '#eb2f96'}}>{x.itemNumber * x.cost}</span>
+                            </div>
+                        )}
+                    />
+                    <Column
+                        title={"    "}
+                        align={"center"}
+                        width={"10%"}
+                        ellipsis={true}
+                        render={(_: any, x: OrderProduct) => (
+                            <Button style={{padding: 0}} type={"link"}
+                                    onClick={(() => onClickDelete(x.id))}>删除</Button>
+                        )}
+                    />
+                </Table>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className={"container"}>
+                <Table
+                    dataSource={OrderListProducts}
+                >
+                    <Column
+                        title={""}
+                        dataIndex={"itemNumber"}
+                        width={"100%"}
+                        align={"center"}
+                        ellipsis={true}
+                        render={(_: any, x: OrderProduct) => (
+                            <div>
+                            <Flex align="space-between">
+                                <div className={"container"} style={{aspectRatio: "1/1", overflow: "auto", width: "40%"}}>
+                                    <Image src={x.image_url[0]} preview={false} style={{objectFit: "cover", aspectRatio: "1/1"}}/>
+                                </div>
+                                   <Flex
+                                       vertical
+                                       style={{width: "60%"}}
+                                       align="flex-start"
+                                       justify="space-evenly"
+                                   >
+                                       <Text
+                                           strong={true}
+                                           ellipsis={true}
+                                           style={{
+                                               margin: '0px',
+                                               height: '20%',
+                                           }}
+                                       >
+                                           {x.product.product_name}
+                                       </Text>
+                                       <Paragraph
+                                           type={"secondary"}
+                                           style={{
+                                               margin: '0px',
+                                               fontSize: "12px", color: "gray",
+                                               whiteSpace: "pre-wrap",
+                                               height: '30%'
+                                           }}
+                                           ellipsis={{rows: 2, expandable: false}}
+                                       >
+                                           {x.product.detailed_product_name}
+                                       </Paragraph>
+
+                                       <Flex
+                                           justify="space-between"
+                                       >
+                                           <div style={{display: 'flex', alignItems: "center", justifyContent: "center"}}>
+                                               <AxCoin size={22}/> <span style={{color: '#eb2f96'}}>{x.cost}</span>
+                                           </div>
+                                           <Space.Compact style={{width: '45%'}}>
+                                               <Button
+                                                   style={{width: '25%', padding: 0}}
+                                                   // size={"small"}
+                                                   // type={"ghost"}
+                                                   // style={{paddingLeft: '10%', paddingRight: '10%'}}
+                                                   onClick={() => onItemNumberMinus(x.id)}
+                                                   disabled={x.itemNumber <= 1}
+                                               >
+                                                   -
+                                               </Button>
+                                               <InputNumber
+                                                   style={{width: '50%'}}
+                                                   // size={"small"}
+                                                   min={1} max={x.stock} value={x.itemNumber}
+                                                   controls={false}
+                                                   onChange={value => {
+                                                       if (value !== null) {
+                                                           onItemNumberChange(x.id, value);
+                                                       }
+                                                   }}
+                                               />
+                                               <Button
+                                                   style={{width: '25%', padding: 0}}
+                                                   // size={"small"}
+                                                   // type={"ghost"}
+                                                   // style={{paddingLeft: '10%', paddingRight: '10%'}}
+                                                   onClick={() => onItemNumberPlus(x.id)}
+                                                   disabled={x.itemNumber >= x.stock}
+                                               >
+                                                   +
+                                               </Button>
+                                           </Space.Compact>
+
+                                           <Button style={{padding: 0}} type={"link"}
+                                                   onClick={(() => onClickDelete(x.id))}>删除
+                                           </Button>
+                                       </Flex>
+                                   </Flex>
+                            </Flex>
+                            </div>
+                        )}
+                    />
+                </Table>
+            </div>
+        )
+    }
+
 }
