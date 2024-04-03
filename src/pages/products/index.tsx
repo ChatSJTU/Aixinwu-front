95bafe020ddf28f326fb9be21bd2fe2d0f931d4b
UNIkeEN <2641712741@qq.com>
2024-04-03 12:14:41 +0800

    feat(products): add category display in products page

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,22 +1,68 @@
 import Head from "next/head";
-import { useState, useEffect} from "react";
-// import Link from "next/link";
-import { Grid, Typography, Row, Col, Space, Segmented, Flex } from "antd";
+import React, { useState, useEffect, useContext } from "react";
+import { Grid, Typography, Row, Col, Menu, Segmented, Flex, Spin, Empty, Divider } from "antd";
+import type { MenuProps } from 'antd';
 import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
+
+// import Link from "next/link";
+import AuthContext from '@/contexts/auth';
+import { MessageContext } from '@/contexts/message';
 import { PageHeader } from "@/components/page-header";
+import { fetchCategories } from "@/services/product";
+import { Category } from "@/models/products";
 
 const { Title } = Typography;
+type MenuItem = Required<MenuProps>['items'][number];
+
+function formatMenuItem (
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: 'group',
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    } as MenuItem;
+}
 
 const ProductsPage = () => {
+    const authCtx = useContext(AuthContext);
+    const client = authCtx.client;
+    const message = useContext(MessageContext);
     const [coinDirection, setCoinDirection] = useState<string>('up');
     const [selectedSortOption, setSelectedSortOption] = useState<string>('default');
-
+    const [categories, setCategories] = useState<Category[] | null>(null);
+    const [currentCategorySlug, setCurrentCategorySlug] = useState<string | null>(null);
+    
     const screens = Grid.useBreakpoint();
 
-    // useEffect(() => {
-    //     console.log(selectedSortOption);
-    // }, [selectedSortOption])
+    useEffect(() => {
+        fetchCategories(client!)
+            .then(res => {
+                setCategories(res);
+                setCurrentCategorySlug(res[0].slug);
+                console.log(res);
+            })
+            .catch(err => message.error(err));
+    }, []);
 
+    // 构建分类菜单项（0/1级）
+    const buildMenuItems = (categories: Category[], levelLimit: number = 2): MenuItem[] =>
+        categories
+            .filter(category => category.level <= levelLimit)
+            .map(category => {
+                if (category.children && category.children.length > 0 && category.level <= levelLimit - 1)
+                    return formatMenuItem(category.name, category.slug, null, buildMenuItems(category.children));
+                return formatMenuItem(category.name, category.slug);
+            }
+    );
+
+    // 排序方式更改辅助函数
     const handleSortChange = (value: string) => {
         if (value.startsWith('price')) {
             if (selectedSortOption === value) {
@@ -46,7 +92,9 @@ const ProductsPage = () => {
         },
     ];
 
-    //TODO 获取商品分类列表与当前分类id
+    if (!categories) {
+        return <center><Spin size="large" style={{ marginTop: '200px' }} /></center>;
+    }
 
     return (
         <>
@@ -58,17 +106,30 @@ const ProductsPage = () => {
                 <Col span={screens.md ? 6 : 24}>
                     <div className="container basic-card">
                         <Title level={5}>分类</Title>
+                        <Divider style={{ marginTop: '-6px', marginBottom: '12px' }} />
+                        <Menu
+                            mode={screens.md ? 'inline' : 'horizontal'}
+                            selectedKeys={[`${currentCategorySlug}`]}
+                            // onClick={handleMenuClick}
+                            style={{ border: 'none' }}
+                            items={buildMenuItems(categories)}
+                        >
+                        </Menu>
+                        {categories.length === 0 &&
+                            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                        }
                     </div>
                 </Col>
                 <Col span={screens.md ? 18 : 24}>
                     <div className="container basic-card">
-                        <Flex align="flex-start" justify="space-between" style={{marginTop: '-6px'}}>
+                        <Flex align="flex-start" justify="space-between" style={{marginTop: '-8px'}}>
                             <Segmented
                                 options={sortOptions}
                                 value={selectedSortOption}
                                 onChange={handleSortChange}
                             />
                         </Flex>
+                        <Divider style={{ marginTop: '12px', marginBottom: '12px' }} />
                     </div>
                 </Col>
             </Row>
