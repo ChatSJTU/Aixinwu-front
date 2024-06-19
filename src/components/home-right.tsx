ed9bd2563289b2733bf7e886915c5f077addba71
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2024-06-19 14:44:56 +0800

    feat(page): add hot-products on homepage

--- src/components/home-right.tsx
+++ src/components/home-right.tsx
@@ -1,11 +1,24 @@
 import { Typography } from "antd";
+import React, { useState, useEffect, useContext } from "react";
 import ProductGrid from "@/components/product-grid";
-import { ProductSummaryExampleList } from "@/models/products";
+import { ProductSummary } from "@/models/products";
 import BasicCard from "@/components/basic-card";
+import AuthContext from '@/contexts/auth';
+import { fetchProductsByCollection } from "@/services/product";
 
 const { Link } = Typography;
 
 export const HomeRightContent = () => {
+
+    const authCtx = useContext(AuthContext);
+    const client = authCtx.client;
+    const [hotProducts, setHotProducts] = useState<ProductSummary[] | null>(null);
+
+    useEffect(() => {
+        fetchProductsByCollection(client!, 12, 'hot-products')
+            .then(products => setHotProducts(products))
+    }, [])
+
     return (
         <>
             <BasicCard title="热门置换" divider
@@ -13,14 +26,14 @@ export const HomeRightContent = () => {
                     <Link href="/products" target="_blank">{'全部>>'}</Link>
                 }
                 >
-                <ProductGrid products={[...ProductSummaryExampleList, ...ProductSummaryExampleList, ...ProductSummaryExampleList]} />
+                <ProductGrid products={hotProducts || []} />
             </BasicCard>
             <BasicCard title="热门租赁" divider
                 titleExtra={
                     <Link href="/products" target="_blank">{'全部>>'}</Link>
                 }
                 >
-                <ProductGrid products={[...ProductSummaryExampleList, ...ProductSummaryExampleList, ...ProductSummaryExampleList]} />
+                <ProductGrid products={[]} />
             </BasicCard>
         </>
     )
