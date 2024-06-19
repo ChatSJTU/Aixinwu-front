c75c38e91782a40a4b1fa1290e6a6607915fe81a
Ichigo2315 <csy_ichigo@sjtu.edu.cn>
2024-06-19 16:09:53 +0800

    fix(tooltip): hide tooltip when only one image exists

--- src/pages/products/single/[slug].tsx
+++ src/pages/products/single/[slug].tsx
@@ -67,54 +67,65 @@ const ProductDetailsPage: React.FC = () => {
                         <Carousel autoplay draggable style={{ textAlign: 'center' }}>
                             {/* Todo: images 可能为 null */}
                             {product.images?.map((url, index) => (
-                                <Tooltip title="拖拽以切换图片" key={index}>
+                                (product.images!.length > 1 && (
+                                    <Tooltip title="拖拽以切换图片" key={index}>
+                                        <Image
+                                            src={url}
+                                            alt={`图片预览 ${index + 1}`}
+                                            width={468}
+                                            height={468}
+                                            preview={false}
+                                        />
+                                    </Tooltip>
+                                )) || (
                                     <Image
+                                        key={index}
                                         src={url}
                                         alt={`图片预览 ${index + 1}`}
                                         width={468}
                                         height={468}
                                         preview={false}
                                     />
-                                </Tooltip>
+                                )
                             ))}
                         </Carousel>
                     </Col>
                     {isMobile &&
-                        <Divider/>
+                        <Divider />
                     }
-                    <Col span={isMobile ? 24 : 15} 
-                        style={isMobile ? {textAlign: 'center'} : { paddingLeft: '150px' }}
+                    <Col span={isMobile ? 24 : 15}
+                        style={isMobile ? { textAlign: 'center' } : { paddingLeft: '150px' }}
                     >
                         <Space direction='vertical' size={'middle'}>
-                            <Title level={2} 
+                            <Title level={2}
                                 style={{ marginTop: '0px' }}>
                                 {product.name}
                             </Title>
                             {
                                 product.detailed_product_name && (
-                                    <Title level={4} 
-                                        style={{ marginTop: '0px' }} 
+                                    <Title level={4}
+                                        style={{ marginTop: '0px' }}
                                         type="secondary">
                                         {product.detailed_product_name}
                                     </Title>
                                 )
                             }
                             {
-                                product.varients.length > 1 &&(
-                                    <Radio.Group 
-                                        onChange={(e)=>{
-                                            setVarient(product.varients.find(x=>x.id == e.target.value));
-                                        }} 
+                                product.varients.length > 1 && (
+                                    <Radio.Group
+                                        onChange={(e) => {
+                                            setVarient(product.varients.find(x => x.id == e.target.value));
+                                        }}
                                         optionType="button">
-                                        {product.varients.map(x=>(
+                                        {product.varients.map(x => (
                                             <Radio.Button value={x.id}>{x.name}</Radio.Button>
                                         ))}
                                     </Radio.Group>
                                 )
                             }
                             <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <Text style={{ fontSize: '16px'}}>售价：&nbsp;</Text>
-                                <AxCoin size={24} value={varient?.price} coloredValue={true}/>
+                                <Text style={{ fontSize: '16px' }}>售价：&nbsp;</Text>
+                                <AxCoin size={24} value={varient?.price} coloredValue={true} />
                             </div>
                             {
                                 varient?.stock && (
@@ -123,16 +134,16 @@ const ProductDetailsPage: React.FC = () => {
                                     </div>
                                 )
                             }
-                            <Flex justify="space-between" 
-                                align="center" 
-                                style={isMobile ? {marginTop: '10px', width: 'auto'} 
-                                        : {marginTop: '50px', width: '400px'}}
+                            <Flex justify="space-between"
+                                align="center"
+                                style={isMobile ? { marginTop: '10px', width: 'auto' }
+                                    : { marginTop: '50px', width: '400px' }}
                             >
                                 <Button
                                     type='primary'
                                     icon={<ShoppingCartOutlined />}
                                     style={{ backgroundColor: '#eb2f96' }}
-                                    onClick={() => {cartCtx.addLines(varient!.id, 1) }}
+                                    onClick={() => { cartCtx.addLines(varient!.id, 1) }}
                                 >
                                     添加到爱心篮
                                 </Button>
