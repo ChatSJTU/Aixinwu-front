2c8b3f9c577155cf173794128b371c76b39769f0
Ichigo2315 <csy_ichigo@sjtu.edu.cn>
2024-09-02 22:08:07 +0800

    fix(quill): fix font display

--- src/components/quill-renderer.tsx
+++ src/components/quill-renderer.tsx
@@ -1,20 +1,25 @@
-import ReactQuill from 'react-quill';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 export interface QuillRendererProps {
-  HTMLContent?: string;
+  HTMLContent: string;
 }
 
 const QuillRenderer: React.FC<QuillRendererProps> = ({ HTMLContent }) => {
+  let fontArr = ['Microsoft-YaHei', 'SimSun', 'SimHei', 'KaiTi', 'FangSong'];
+  let Font = Quill.import('formats/font');
+  Font.whitelist = fontArr;
+  Quill.register(Font, true);
+
   return (
-      <ReactQuill
-        theme={'snow'}
-        value={HTMLContent ?? ''}
-        readOnly={true}
-        modules={{
-          toolbar: false,  // 禁用工具栏
-        }}
-      />
+    <ReactQuill
+      theme={'snow'}
+      value={HTMLContent ?? ''}
+      readOnly={true}
+      modules={{
+        toolbar: false,  // 禁用工具栏
+      }}
+    />
   )
 }
 
